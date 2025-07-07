21907908e8ff69378a79b2ecef09fc3df6ee758a
Albin Kjellberg <albin@kjellbergalbin.se>
2025-07-07 23:31:02 +0200

    Started on player controller

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,6 +5,7 @@ public class PlayerController : MonoBehaviour
 {
     // Player stats
     public float movementSpeed = 5f; // Speed of the player movement
+    public float rotationSpeed = 100f; // Speed of the player rotation
 
     // Reference to the InputAction for player movement
     InputAction moveAction;
@@ -14,19 +15,55 @@ public class PlayerController : MonoBehaviour
 
     // Reference to the Camera for player movement direction
     Camera mainCamera;
+    Vector3 cameraForward;
+    Vector3 cameraRight;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        moveAction = InputActionAsset.FindAction("Move");
         rb = GetComponent<Rigidbody>();
         mainCamera = Camera.main;
+
+        moveAction = InputSystem.actions.FindAction("Player/Move");
+
+        Cursor.lockState = CursorLockMode.Locked; // Lock the cursor to the center of the screen
+        Cursor.visible = false; // Hide the cursor
     }
 
     // Update is called once per frame
     void Update()
     {
+        getCameraVectors();
+        rotatePlayer();
         movePlayer();
+    }
+
+    void getCameraVectors()
+    {
+        // Get the camera's forward and right vectors
+        cameraForward = mainCamera.transform.forward;
+        cameraRight = mainCamera.transform.right;
+
+        // Ignore the vertical component of the camera vectors
+        cameraForward.y = 0;
+        cameraRight.y = 0;
+
+        // Normalize the vectors to ensure they have a magnitude of 1
+        cameraForward.Normalize();
+        cameraRight.Normalize();
+    }
+
+    void rotatePlayer()
+    {
+        // Get current rotation
+        float playerRotationY = transform.rotation.eulerAngles.y;
+        Debug.Log($"Current Player Rotation: {playerRotationY}");
+
+        // Get camera rotation
+        float cameraRotationY = mainCamera.transform.rotation.eulerAngles.y;
+        Debug.Log($"Current Camera Rotation: {cameraRotationY}");
+
+
     }
 
     void movePlayer()
@@ -38,14 +75,6 @@ public class PlayerController : MonoBehaviour
         {
             moveDirection.Normalize();
         }
-
-        // Get the camera's forward and right vectors        
-        Vector3 cameraForward = mainCamera.transform.forward;
-        Vector3 cameraRight = mainCamera.transform.right;
-        cameraForward.y = 0; // Ignore vertical component
-        cameraRight.y = 0; // Ignore vertical component
-        cameraForward.Normalize();
-        cameraRight.Normalize();
 
         // Calculate the final movement direction based on camera orientation
         Vector3 finalDirection = cameraForward * moveDirection.z + cameraRight * moveDirection.x;
