90d3a7acd71c2e00abffaceebb5a1cb3fff2470c
Albin Kjellberg <albin@kjellbergalbin.se>
2025-07-09 23:55:28 +0200

    Added behaviour tree

--- Assets/Scripts/Enemy/EnemyController.cs
+++ Assets/Scripts/Enemy/EnemyController.cs
@@ -1,16 +1,29 @@
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private Animator _animator;
+    private NavMeshAgent _agent;
+    [SerializeField] private float movementSpeed = 5f;
+    // [SerializeField] private float rotationSpeed = 5f;
+
+    void Awake()
     {
-        
+        _animator = GetComponent<Animator>();
+        _agent = GetComponent<NavMeshAgent>();
+        _agent.speed = movementSpeed;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        ApplyAnimation();
+    }
+
+    private void ApplyAnimation()
+    {
+        // Animate the enemy based on its movement
+        _animator.SetFloat("Speed", _agent.velocity.magnitude);
     }
 }
